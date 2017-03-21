244a59550fd8779efb6f641a560177339da0815f
Olexyy <inua@ua.fm>
2017-03-21 23:57:21 +0200

    Added buttons handlers implementation.

--- Calculator/CalculatorForm.cs
+++ Calculator/CalculatorForm.cs
@@ -27,106 +27,107 @@ namespace Calculator
                 this.ExtendedMode = true;
             else
                 this.ExtendedMode = false;
+            this.Input = this.textBoxExpression.Text;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "1";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "2";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "3";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "4";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "5";
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "6";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "7";
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "8";
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "9";
         }
 
         private void button0_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "0";
         }
 
         private void buttonDivide_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "/";
         }
 
         private void buttonMultiply_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "*";
         }
 
         private void buttonMinus_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "-";
         }
 
         private void buttonPlus_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "+";
         }
 
         private void buttonNegate_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "-";
         }
 
         private void buttonMod_Click(object sender, EventArgs e)
         {
-
+            this.textBoxExpression.Text += "%";
         }
 
         private void buttonMemoryRecall_Click(object sender, EventArgs e)
         {
-
+            // todo: implement
         }
 
         private void buttonMemoryPlus_Click(object sender, EventArgs e)
         {
-
+            // todo: implement
         }
 
         private void buttonMemoryClear_Click(object sender, EventArgs e)
         {
-
+            // todo: implement
         }
 
         private void buttonCalculate_Click(object sender, EventArgs e)
         {
-
+            // todo: implement
         }
     }
 }
